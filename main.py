4fe91e785430cc2f01743a42ba1cb6537151bf34
toxicbullets <jtp292@vt.edu>
2017-10-07 12:27:41 -0400

    Added command line input to main function

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import boto3
+import writer
+value = raw_input("Enter retrieved value:  ")
+writer.ProcessandPrint(int(value))
+
--- writer.py
+++ writer.py
@@ -1,4 +1,3 @@
-import boto3
 def printFunctionChange(found, data,f, path):
 	for i in range(0,found+1):
 		if i == found:
@@ -9,30 +8,30 @@ def printFunctionChange(found, data,f, path):
 		f.write(data[i])
 	f.close
 		
-		
-retrievedValue = 3;
-path = 'C:\Users\Jordan\Documents\Hackathon\Assets\AWSScript.cs'
-count = 0
-found = 0
-data = []
-with open(path, 'r') as f:
-	for line in f:
-		if "Badgers" in line:
-			found = count
-		else:
-			data.append(line)
-		count += 1;
-with open(path, 'w') as f:
-	textPath = ''
-	if retrievedValue == 0:
-		textPath = 'spawnCube.txt'
-	elif retrievedValue == 1:
-		textPath = 'addTexture.txt'
-	elif retrievedValue == 2:
-		textPath = 'addSurroundings.txt'
-	else: 
-		textPath = 'startMovie.txt'
-	printFunctionChange(found, data,f, textPath)
+def ProcessandPrint(retrievedValue)	:
+	print (retrievedValue)
+	path = 'C:\Users\Jordan\Documents\Hackathon\Assets\AWSScript.cs'
+	count = 0
+	found = 0
+	data = []
+	with open(path, 'r') as f:
+		for line in f:
+			if "Badgers" in line:
+				found = count
+			else:
+				data.append(line)
+			count += 1;
+	with open(path, 'w') as f:
+		textPath = ''
+		if retrievedValue == 0:
+			textPath = 'spawnCube.txt'
+		elif retrievedValue == 1:
+			textPath = 'addTexture.txt'
+		elif retrievedValue == 2:
+			textPath = 'addSurroundings.txt'
+		else: 
+			textPath = 'startMovie.txt'
+		printFunctionChange(found, data,f, textPath)
 			
 			
 
